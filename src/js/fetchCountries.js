52fb0c326aaee1ee23543e7aa3000ed142432c9a
333Nikita333 <nikitosfox333@gmail.com>
2023-01-08 19:18:26 +0200

    add field filtering

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,9 @@
-const BASE_URL = 'https://restcountries.com/v3.1/';
+const BASE_URL = 'https://restcountries.com/v3.1';
 
 function fetchCountry(countryName) {
-  return fetch(`${BASE_URL}/name/${countryName}`).then(response => {
+  return fetch(
+    `${BASE_URL}/name/${countryName}?fields=name,capital,population,flags,languages`
+  ).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
